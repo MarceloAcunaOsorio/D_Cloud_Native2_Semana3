a767b9fa58149e9eac292b4d270666dd89af15b5
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2025-03-30 20:43:58 -0300

    se carga controlador

---
+++ Backend/src/main/java/com/example/Backend/Controller/AuthController.java
@@ -0,0 +1,63 @@
+package com.example.Backend.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.http.HttpHeaders;
+
+import com.example.Backend.DTO.JwtResponseDto;
+import com.example.Backend.DTO.LoginDto;
+import com.example.Backend.DTO.RegisterDto;
+import com.example.Backend.DTO.UserDTO;
+import com.example.Backend.Security.JwtGenerator;
+import com.example.Backend.Service.RolService;
+import com.example.Backend.Service.UserService;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+    
+    @Autowired
+    private RolService rolService;
+    
+    @Autowired
+    private JwtGenerator jwtGenerator;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponseDto> login(@RequestBody LoginDto loginDto) {
+        return ResponseEntity.ok(userService.login(loginDto));
+    }
+    
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+        userService.register(registerDto);
+
+        return new ResponseEntity<>("User register success!", HttpStatus.CREATED);
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<?> refreshToke(Authentication authentication){
+
+        String token = jwtGenerator.refreshToken(authentication);
+
+        JwtResponseDto jwtRefresh = new JwtResponseDto(token);
+        return new ResponseEntity<JwtResponseDto>(jwtRefresh, HttpStatus.OK);
+    }
+    @GetMapping("/logued")
+    public ResponseEntity<UserDTO> getLoguedUser(@RequestHeader HttpHeaders headers){
+        return new ResponseEntity<>(userService.getLoguedUser(headers), HttpStatus.OK);
+    }
+    
+}
